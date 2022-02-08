ad40e0e86213b797cbb3878f04f31e7432825e38
Jonathan Smith <jonathan.smith@semsee.com>
2022-02-08 00:05:38 -0400

    calling the login api

--- ApiFetch/App.js
+++ ApiFetch/App.js
@@ -1,26 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 
-const movieUrl = "https://reactnative.dev/movies.json"
+//const apiUrl = "https://reactnative.dev/movies.json"
+const apiUrl = "https://ds2.9packsoftware.com/9PackXRFS/Vision/Live/ValidateUser/124/124?ClientCode=9pack&AppVersion=21.02.0010&RequestDateTime=01/20/2022&ProductType=Vision&format=json"
 
 const App = () => {
 
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch(movieUrl)
+    fetch(apiUrl)
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error));
   }, [])
 
   // console.log("this is a test text")
-  // console.log(data)
-
+  // console.log(data.payLoad.entities[0].items[0].isAuthenticated)
+  const value = data.payLoad.entities[0].items[0].isAuthenticated
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>
-        {data.description}
+         Is authenticated: {String(value)} 
       </Text>
     </View>
   );
